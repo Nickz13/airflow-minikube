5909c3a46bcce647a2e6628d783b855a468114ee
Nitin Tundwal <31153647+Nickz13@users.noreply.github.com>
2022-03-04 15:00:54 +1100

    updated

--- dags_transformandanalysis.py
+++ dags_transformandanalysis.py
@@ -18,35 +18,36 @@ default_args = {
 dag = DAG('kubernetes_pod_example',default_args=default_args, schedule_interval=None)
 
 # Task
+
 with dag:
-      task1 = KubernetesPodOperator(
-                       task_id="dbt-transform",
-                       name="dbt-transform",
-                       namespace="airflow",
-                       image="170108258435.dkr.ecr.ap-southeast-2.amazonaws.com/dbt_image_test:latest",
-                       cmds=["dbt"],
-                       arguments=["run", "--models transform", "--profiles-dir", "."],
-                       ## no change on below
-                       get_logs=True,
-                       dag=dag,
-                       is_delete_operator_pod=False,
-                     #   config_file=KUBE_CONFIG,
-                       in_cluster=True,
+    task_1 = KubernetesPodOperator(
+        task_id="dbt-transform",
+        name="dbt-transform",
+        namespace="airflow",
+        image="170108258435.dkr.ecr.ap-southeast-2.amazonaws.com/dbt_image_test:latest",
+        cmds=["dbt"],
+        arguments=["run", "--models transform", "--profiles-dir", "."],
+        ## no change on below
+        get_logs=True,
+        dag=dag,
+        is_delete_operator_pod=False,
+        #   config_file=KUBE_CONFIG,
+        in_cluster=True,
                      #   service_account_name=KUBE_SERVICE_ACCOUNT
-                       )
-       task2 = KubernetesPodOperator(
-                          task_id="dbt-analysis",
-                          name="dbt-analysis",
-                          namespace="airflow",
-                          image="170108258435.dkr.ecr.ap-southeast-2.amazonaws.com/dbt_image_test:latest",
-                          cmds=["dbt"],
-                          arguments=["run", "--models analysis", "--profiles-dir", "."],
-                          ## no change on below
-                          get_logs=True,
-                          dag=dag,
-                          is_delete_operator_pod=False,
-                        #   config_file=KUBE_CONFIG,
-                          in_cluster=True,
-                        #   service_account_name=KUBE_SERVICE_ACCOUNT
-                          )
-task1>>task2
+    )
+    task_2 = KubernetesPodOperator(
+        task_id="dbt-analysis",
+        name="dbt-analysis",
+        namespace="airflow",
+        image="170108258435.dkr.ecr.ap-southeast-2.amazonaws.com/dbt_image_test:latest",
+        cmds=["dbt"],
+        arguments=["run", "--models analysis", "--profiles-dir", "."],
+        ## no change on below
+        get_logs=True,
+        dag=dag,
+        is_delete_operator_pod=False,
+        #   config_file=KUBE_CONFIG,
+        in_cluster=True,
+    )
+
+task_1 >> task_2
